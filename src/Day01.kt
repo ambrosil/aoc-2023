2e684bae88c6b24aa2a63e839aa107a81ecce20c
Luca Ambrosi <luca@luca.it>
2023-12-01 07:40:05 +0100

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,13 +1,42 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .map { it.filter { c -> c.isDigit() } }
+            .sumOf { "${it.first()}${it.last()}".toInt() }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val spellingMap = mapOf(
+            "one" to "1", "two" to "2",
+            "three" to "3", "four" to "4",
+            "five" to "5", "six" to "6",
+            "seven" to "7", "eight" to "8", "nine" to "9"
+        )
+
+        fun String.fixSpelling(): String {
+            var res = ""
+
+            this.forEachIndexed { i, c ->
+                if (c.isDigit()) res += c
+
+                spellingMap.entries.forEach {
+                    if (this.length >= i + it.key.length) {
+                        val substring = this.substring(i, i + it.key.length)
+                        if (substring == it.key) res += it.value
+                    }
+                }
+            }
+
+            return res
+        }
+
+        return input
+            .map { it.fixSpelling().filter { c -> c.isDigit() } }
+            .sumOf { "${it.first()}${it.last()}".toInt() }
     }
 
     val input = readInput("inputs/Day01")
     part1(input).println()
     part2(input).println()
 }
+
