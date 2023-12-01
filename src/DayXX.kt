b54bd171e7afb897fe8dd1ae8834f672977f4218
ambrosil <luca.ambrosi@qooa.it>
2023-12-01 10:17:45 +0100

    DayXX

---
+++ src/DayXX.kt
@@ -0,0 +1,14 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 1
+    }
+
+    fun part2(input: List<String>): Int {
+        return 1
+    }
+
+    val input = readInput("inputs/DayXX")
+    part1(input).println()
+    part2(input).println()
+}
+
