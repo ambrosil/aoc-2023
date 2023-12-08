0023d5220e0aab954bbcd315cf894190a17dbc39
Luca Ambrosi <luca@luca.it>
2023-12-08 15:07:28 +0100

    Day 8

---
+++ src/Day08.kt
@@ -0,0 +1,54 @@
+import utils.leastCommonMultiple
+
+fun main() {
+
+    data class Node(val left: String, val right: String)
+
+    fun parseInput(input: List<String>): Pair<String, MutableMap<String, Node>> {
+        val directions = input.first()
+        val network = input.drop(2).fold(mutableMapOf<String, Node>()) { acc, i ->
+            val nodeName = i.split(" = ").first().trim()
+            val (left, right) = i.replace("(", "")
+                .replace(")", "")
+                .split(" = ")
+                .last()
+                .split(", ")
+
+            acc[nodeName] = Node(left, right)
+            acc
+        }
+
+        return directions to network
+    }
+
+    fun findZ(startNode: String, directions: String, network: MutableMap<String, Node>, testFunc: (current: String) -> Boolean): Int {
+        var counter = 0
+        var current = startNode
+
+        while (testFunc(current)) {
+            directions.forEach {
+                current = if (it == 'R') network[current]!!.right else network[current]!!.left
+            }
+            counter += directions.length
+        }
+
+        return counter
+    }
+
+    fun part1(input: List<String>): Int {
+        val (directions, network) = parseInput(input)
+        return findZ("AAA", directions, network) { it != "ZZZ" }
+    }
+
+    fun part2(input: List<String>): Int {
+        val (directions, network) = parseInput(input)
+        return network.keys.filter { it.endsWith("A") }
+            .map { findZ(it, directions, network) { !it.endsWith("Z") } }
+            .reduce { acc, i -> leastCommonMultiple(acc, i) }
+    }
+
+    val input = readInput("inputs/Day08")
+    part1(input).println()
+    part2(input).println()
+}
+
